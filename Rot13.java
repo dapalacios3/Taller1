685fdc8dc43a58642a6d331be7bb36fec860195a
dapalacios3 <32887901+dapalacios3@users.noreply.github.com>
2017-10-17 22:32:52 -0500

    Método para codificar un texto a Rot13

---
+++ Rot13.java
@@ -0,0 +1,13 @@
+public String getRot13() {
+        String s = texto;
+        String rot13 = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            rot13+=c;
+        }
+        return rot13;
+    }
